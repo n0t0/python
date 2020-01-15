84205a52454b78f11b2b3d71507e4b66da82e4ce
Ivo Ivanov <vn.n0t0@gmail.com>
2020-01-14 20:27:46 -0800

    file - update

---
+++ Exercises/hackerrank/01_sock_merchant.py
@@ -0,0 +1,14 @@
+import math
+import itertools
+
+
+colors = [1, 2, 1, 2, 1, 3, 2]
+letters = ['a', 'b', 'a', 'd', 'e', 'b']
+
+# sockMerchant():
+# for i, j in enumerate(colors[:-1]):
+#     #     # if the sum of same numbers in colors is even devide 2 to get pairs
+#     if j == colors[i + 1]:
+#         colors[i] = "foo"
+#         colors[i+1] = "foo"
+# print(colors)
