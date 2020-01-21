2c915a350e60594625bce87518ada223a661030e
Ivo Ivanov <vn.n0t0@gmail.com>
2020-01-20 20:59:37 -0800

    day 4 completed

--- Exercises/hackerrank/04_class-vs-instance.py
+++ Exercises/hackerrank/04_class-vs-instance.py
@@ -1,20 +1,25 @@
 class Person:
     def __init__(self, initialAge):
         # Add some more code to run some checks on initialAge
+        if initialAge < 0:
+            print("Age is not valid, setting age to 0.")
+            self.age = 0
+        else:
+            self.age = initialAge
 
-    def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
-        if age < 13:
+    def amIOld(self, age):
+        # Do some computations in here and print out the correct statement to
+        # the console
+        if self.age < 13:
             print("You are young.")
-        elif age >= 13 and age < 18:
-            print("You are teenager.")
+        elif (self.age >= 13) and (self.age < 18):
+            print("You are a teenager.")
         else:
             print("You are old.")
 
-    def yearPasses(self):
+    def yearPasses(self, age):
         # Increment the age of the person in here
-        while age > 0:
-            return += 1
+        self.age += 1
 
 
 t = int(input())
--- Exercises/self/classes052817.py
+++ Exercises/self/classes052817.py
@@ -9,16 +9,17 @@ class Music():
         self.song = song
 
     def playMusic(self):
-        print 'ARTIST:\t %s\n ALBUM:\t %s\n SONG:\t %s' % (self.artist, self.album, self.song)
+        print ('ARTIST:\t %s\n ALBUM:\t %s\n SONG:\t %s' %
+               (self.artist, self.album, self.song))
 
 
-
-player_1 = Music('Scorpions','Savage Amusement','Every Minute Every Day')
-player_2 = Music('Scorpions',"Best of Rockers 'N' Ballads",'Still Loving You')
+player_1 = Music('Scorpions', 'Savage Amusement', 'Every Minute Every Day')
+player_2 = Music('Scorpions', "Best of Rockers 'N' Ballads",
+                 'Still Loving You')
 
 
 player_1.playMusic()
 player_2.playMusic()
 
-print player_1.instrument
-print player_2.style
+print (player_1.instrument)
+print (player_2.style)
